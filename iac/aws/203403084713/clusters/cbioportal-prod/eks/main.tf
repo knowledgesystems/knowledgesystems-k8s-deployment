ea0ec94e59ee115a006927340a4a0a5fbc341b30
Zain Nasir <zainasir1999@gmail.com>
2025-02-24 14:28:58 -0500

    update helm values

--- iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
@@ -34,10 +34,24 @@ module "eks_cluster" {
     }
     argocd = {
       instance_types = ["m5.large"]
-      ami_type      = "BOTTLEROCKET_x86_64"
+      ami_type       = "BOTTLEROCKET_x86_64"
       desired_size   = 1
       max_size       = 1
       min_size       = 1
+    }
+    redis = {
+      instance_types = ["r7g.large"]
+      ami_type       = "BOTTLEROCKET_ARM_64"
+      desired_size   = 1
+      max_size       = 1
+      min_size       = 1
+      taints = {
+        dedicated = {
+          key    = "workload"
+          value  = "cbioportal-redis"
+          effect = "NO_SCHEDULE"
+        }
+      }
     }
   }
 }
