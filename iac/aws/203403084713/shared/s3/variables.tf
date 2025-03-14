d19d94e089814d446da2da1250d3270b182e4810
Zain Nasir <zainasir1999@gmail.com>
2025-03-14 15:58:58 -0400

    migrate public database dump to new cluster

---
+++ iac/aws/203403084713/shared/s3/variables.tf
@@ -0,0 +1,22 @@
+variable "AWS_PROFILE" {
+  description = "AWS cli profile"
+  type        = string
+  ephemeral   = true
+  default     = "default"
+}
+
+variable "AWS_REGION" {
+  description = "AWS cli region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "AWS_DEFAULT_TAGS" {
+  description = "Set of default tags that get added to all resources"
+  type        = map(string)
+  default = {
+    cdsi-owner = "nasirz1@mskcc.org"
+    cdsi-app   = "cbioportal"
+    cdsi-team  = "data-visualization"
+  }
+}
