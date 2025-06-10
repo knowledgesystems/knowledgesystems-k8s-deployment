61e4c9f449cb1c335cc4bb839404f0b39924f7b4
Zain Nasir <zainasir1999@gmail.com>
2025-06-10 09:01:46 -0400

    upgrade cbioportal nodegroup to match new nodegroup sizes

--- iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
@@ -4,9 +4,9 @@ locals {
     cbioportal = {
       instance_types = ["r7g.xlarge"]
       ami_type       = "BOTTLEROCKET_ARM_64"
-      desired_size   = 1
-      max_size       = 2
-      min_size       = 1
+      desired_size   = 4
+      max_size       = 5
+      min_size       = 4
       taints = {
         dedicated = {
           key    = var.TAINT_KEY
