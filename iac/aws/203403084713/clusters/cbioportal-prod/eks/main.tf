3a8c590feac712c849a5514203990dc3bbb4672c
Zain Nasir <zainasir1999@gmail.com>
2025-06-12 15:18:24 -0400

    update terraform module source for 2034 cbio cluster

--- iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
@@ -141,7 +141,7 @@ locals {
 }
 
 module "eks_cluster" {
-  source       = "git::ssh://git@github.com/cBioPortal/terraform.git//src/module/hyc-eks?ref=main"
+  source       = "git::https://github.com/MSK-Staging/cbioportal-terraform.git//src/module/hyc-eks?ref=feature/modularize-base"
   cluster_name = var.CLUSTER_NAME
 
   # General EKS Config
