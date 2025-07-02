ef350d35dbd1c79e57290ac51a5a9e8b04917665
Zain Nasir <zainasir1999@gmail.com>
2025-07-01 22:08:44 -0400

    increase resources for genie persistence redis

--- iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
@@ -62,9 +62,9 @@ locals {
     redis = {
       instance_types = ["r7g.large"]
       ami_type       = "BOTTLEROCKET_ARM_64"
-      desired_size   = 3
-      max_size       = 4
-      min_size       = 2
+      desired_size   = 5
+      max_size       = 7
+      min_size       = 5
       taints = {
         dedicated = {
           key    = var.TAINT_KEY
