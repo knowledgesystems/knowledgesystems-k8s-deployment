44d8d3e232063ed87f61a7830145269e1d5f8261
Zain Nasir <zainasir1999@gmail.com>
2025-07-15 09:39:10 -0400

    increase node count for cbio-dev aws eks nodegroup

--- iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
@@ -38,9 +38,9 @@ locals {
     cbio-dev = {
       instance_types = ["r7g.medium"]
       ami_type       = "BOTTLEROCKET_ARM_64"
-      desired_size   = 4
-      max_size       = 4
-      min_size       = 3
+      desired_size   = 5
+      max_size       = 6
+      min_size       = 4
       taints = {
         dedicated = {
           key    = var.TAINT_KEY
