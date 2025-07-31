a8aa6050f0098a2b1481d750f92da2929bd1384b
Zain Nasir <zainasir1999@gmail.com>
2025-07-31 14:17:08 -0400

    add terraform to cbio-dev

---
+++ iac/aws/397229547211/clusters/cbioportal-dev/eks/main.tf
@@ -0,0 +1,48 @@
+locals {
+  # Use locals for node groups to enforce required tags
+  node_groups = {
+    argocd = {
+      instance_types = ["m5.large"]
+      ami_type       = "BOTTLEROCKET_x86_64"
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 2
+      block_device_mappings = {
+        root_vol = var.ROOT_VOL_CONFIG
+      }
+    }
+  }
+}
+
+module "eks_cluster" {
+  source       = "git::https://github.com/MSK-Staging/terraform-aws-hyc-eks.git"
+  cluster_name = var.CLUSTER_NAME
+
+  # General EKS Config
+  cluster_version = var.CLUSTER_VER
+  tags = {
+    Environment = var.CLUSTER_ENV
+  }
+
+  # Network Config
+  vpc_id                   = var.VPC_ID
+  control_plane_subnet_ids = var.CONTROL_PLANE_SUBNET_IDS
+  subnet_ids               = var.SUBNET_IDS
+
+  # API Controls
+  cluster_endpoint_public_access  = var.API_PUBLIC
+  cluster_endpoint_private_access = var.API_PRIVATE
+
+  # EKS Managed Node Groups
+  eks_managed_node_groups = {
+    for name, config in local.node_groups : name => merge(config, {
+      cluster_version = try(config.version, var.NODEGROUP_VER)
+      tags = merge(
+        try(config.tags, {}),
+        {
+          "nodegroup-name" = name
+        }
+      )
+    })
+  }
+}
---
+++ iac/aws/397229547211/clusters/cbioportal-dev/eks/variables.tf
@@ -0,0 +1,108 @@
+variable "AWS_PROFILE" {
+  description = "AWS cli profile"
+  type        = string
+  ephemeral   = true
+  default     = "default"
+}
+
+variable "AWS_REGION" {
+  description = "AWS cli region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "AWS_DEFAULT_TAGS" {
+  description = "Set of default tags that get added to all resources. Avoid changing this as this updates a lot of resources and could lead to disruption. As an alternative, change tags for the individual resources."
+  type        = map(string)
+  default = {
+    cdsi-owner = "nasirz1@mskcc.org"
+    cdsi-app   = "cbioportal"
+    cdsi-team  = "data-visualization"
+  }
+}
+
+variable "CLUSTER_NAME" {
+  description = "Name of the cluster"
+  type        = string
+  default     = "cbioportal-dev"
+}
+
+variable "CLUSTER_VER" {
+  description = "Kubernetes version of the cluster"
+  type        = string
+  default     = "1.32"
+}
+
+variable "NODEGROUP_VER" {
+  description = "Kubernetes version of the nodegroups"
+  type        = string
+  default     = "1.32"
+}
+
+variable "CLUSTER_ENV" {
+  description = "Environment value for the cluster [prod | dev]"
+  type        = string
+  default     = "dev"
+}
+
+variable "VPC_ID" {
+  description = "VPC id for the cluster"
+  type        = string
+  default     = "vpc-054a7c74dc0b761a9"
+}
+
+variable "CONTROL_PLANE_SUBNET_IDS" {
+  description = "A list of subnet IDs where the EKS cluster control plane (ENIs) will be provisioned"
+  type        = list(string)
+  default     = ["subnet-0e91c0e7119c7a136", "subnet-04197dcac390f0a0f", "subnet-0b543497e4043b54e"]
+}
+
+variable "SUBNET_IDS" {
+  description = "A list of subnet IDs where the nodes/node groups will be provisioned"
+  type        = list(string)
+  default     = ["subnet-0e91c0e7119c7a136", "subnet-04197dcac390f0a0f", "subnet-0b543497e4043b54e"]
+}
+
+variable "API_PUBLIC" {
+  description = "Enable/Disable public api endpoint for the cluster"
+  type        = bool
+  default     = true
+}
+
+variable "API_PRIVATE" {
+  description = "Enable/Disable private api endpoint for the cluster"
+  type        = bool
+  default     = false
+}
+
+variable "LABEL_KEY" {
+  description = "Default key for node labels"
+  type        = string
+  default     = "workload"
+}
+
+variable "ROOT_VOL_CONFIG" {
+  description = "This is the custom root disk config for nodegroups that need a larger disk size"
+  type = object({
+    device_name = string
+    ebs = object({
+      volume_size           = number
+      volume_type           = string
+      iops                  = number
+      throughput            = number
+      encrypted             = bool
+      delete_on_termination = bool
+    })
+  })
+  default = {
+    device_name = "/dev/xvda"
+    ebs = {
+      volume_size           = 80
+      volume_type           = "gp3"
+      iops                  = 3000
+      throughput            = 125
+      encrypted             = true
+      delete_on_termination = true
+    }
+  }
+}
