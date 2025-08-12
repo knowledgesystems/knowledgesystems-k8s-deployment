78964550698bd823fa612ffa3cf01c4b84a4fd88
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2025-08-11 20:42:18 -0400

    Downsize oncokb msk prod nodegroup and upgrade to t4g instances

--- iac/aws/084637913395/clusters/oncokb-production/eks/main.tf
+++ iac/aws/084637913395/clusters/oncokb-production/eks/main.tf
@@ -2,13 +2,30 @@ locals {
   # Use locals for node groups to enforce required tags
   node_groups = {
     lg-general = {
-      instance_types = ["t3.2xlarge"]
-      ami_type       = "BOTTLEROCKET_x86_64"
-      desired_size   = 2
-      max_size       = 2
-      min_size       = 2
+      instance_types = ["t4g.xlarge"]
+      ami_type       = "BOTTLEROCKET_ARM_64"
+      desired_size   = 3
+      max_size       = 3
+      min_size       = 3
       labels = {
         (var.LABEL_KEY) = "lg-general"
+      }
+    }
+    legacy-apps = {
+      instance_types = ["t3.medium"]
+      ami_type       = "BOTTLEROCKET_x86_64"
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
+      taints = {
+        dedicated = {
+          key    = "workload"
+          value  = "legacy-apps"
+          effect = "NO_SCHEDULE"
+        }
+      }
+      labels = {
+        (var.LABEL_KEY) = "legacy-apps"
       }
     }
   }
