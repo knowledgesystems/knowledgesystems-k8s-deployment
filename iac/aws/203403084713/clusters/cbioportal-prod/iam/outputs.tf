0c965500ea0e26d9048800304a341d08effbe8a8
Zain Nasir <zainasir1999@gmail.com>
2025-08-14 11:32:34 -0400

    refactor iam module for cbioportal public cluster

--- iac/aws/203403084713/clusters/cbioportal-prod/iam/outputs.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/iam/outputs.tf
@@ -1,4 +1,4 @@
 output "cellxgene_s3_mountpoint_role_arn" {
-  value       = aws_iam_role.userServiceRoleCellxgeneS3Mountpoint
+  value       = aws_iam_role.userServiceRoleCellxgeneS3Mountpoint.arn
   description = "ARN of the IAM Role for the cellxgene s3 mountpoint"
 }
---
+++ iac/aws/203403084713/clusters/cbioportal-prod/iam/variables.tf
@@ -0,0 +1,4 @@
+variable "cluster_oidc_provider_arn" {
+  description = "The OIDC provider ARN for the EKS cluster"
+  type        = string
+}
