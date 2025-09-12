8a484f5961957db9090df984afa9ade0756255be
Zain Nasir <zainasir1999@gmail.com>
2025-09-12 16:41:48 -0400

    modify ingress values

--- iac/aws/666628074417/clusters/cbioportal-prod/eks/variables.tf
+++ iac/aws/666628074417/clusters/cbioportal-prod/eks/variables.tf
@@ -33,6 +33,12 @@ variable "CLUSTER_VER" {
   default     = "1.32"
 }
 
+variable "NODEGROUP_VER" {
+  description = "Kubernetes version of the nodegroups"
+  type        = string
+  default     = "1.32"
+}
+
 variable "CLUSTER_ENV" {
   description = "Environment value for the cluster [prod | dev]"
   type        = string
@@ -73,4 +79,68 @@ variable "LABEL_KEY" {
   description = "Default key for node labels"
   type        = string
   default     = "workload"
+}
+
+variable "TAINT_KEY" {
+  description = "Default key for taints"
+  type        = string
+  default     = "workload"
+}
+
+variable "TAINT_EFFECT" {
+  description = "Default effect for taints"
+  type        = string
+  default     = "NO_SCHEDULE"
+}
+
+variable "ROOT_VOL_CONFIG" {
+  description = "This is the custom root disk config for nodegroups that need a larger disk size"
+  type = object({
+    device_name = string
+    ebs = object({
+      volume_size           = number
+      volume_type           = string
+      iops                  = number
+      throughput            = number
+      encrypted             = bool
+      delete_on_termination = bool
+    })
+  })
+  default = {
+    device_name = "/dev/xvda"
+    ebs = {
+      volume_size           = 80
+      volume_type           = "gp3"
+      iops                  = 3000
+      throughput            = 125
+      encrypted             = true
+      delete_on_termination = true
+    }
+  }
+}
+
+variable "DATA_VOL_CONFIG" {
+  description = "This is the custom root disk config for nodegroups that need a larger disk size"
+  type = object({
+    device_name = string
+    ebs = object({
+      volume_size           = number
+      volume_type           = string
+      iops                  = number
+      throughput            = number
+      encrypted             = bool
+      delete_on_termination = bool
+    })
+  })
+  default = {
+    device_name = "/dev/xvdb"
+    ebs = {
+      volume_size           = 50
+      volume_type           = "gp3"
+      iops                  = 3000
+      throughput            = 125
+      encrypted             = true
+      delete_on_termination = true
+    }
+  }
 }
