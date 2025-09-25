e1de7fa78a3d438da532db7c6b5df291f93c9996
Zain Nasir <zainasir1999@gmail.com>
2025-09-25 11:25:04 -0400

    create service catalog s3 product for cbioportal-datahub

--- iac/aws/203403084713/shared/s3/main.tf
+++ iac/aws/203403084713/shared/s3/main.tf
@@ -5,7 +5,7 @@ resource "aws_servicecatalog_provisioned_product" "cBioPortal_Public_DB_Dump" {
 
   provisioning_parameters {
     key   = "ACMCertificateArn"
-    value = "arn:aws:acm:us-east-1:203403084713:certificate/b3a29c0f-0e1a-4b58-b4ba-32d38492a16a"
+    value = var.S3_CDN_CERT_ARN
   }
 
   provisioning_parameters {
@@ -19,10 +19,38 @@ resource "aws_servicecatalog_provisioned_product" "cBioPortal_Public_DB_Dump" {
   }
 }
 
+resource "aws_servicecatalog_provisioned_product" "cbioportal-datahub" {
+  name                       = "cbioportal-datahub"
+  product_id                 = "prod-5ghnhr2n5wnx4"
+  provisioning_artifact_name = "1.3.5"
+
+  provisioning_parameters {
+    key   = "ACMCertificateArn"
+    value = var.S3_CDN_CERT_ARN
+  }
+
+  provisioning_parameters {
+    key   = "DomainName"
+    value = "datahub.assets.cbioportal.org"
+  }
+
+
+  provisioning_parameters {
+    key   = "Block"
+    value = "false"
+  }
+
+  provisioning_parameters {
+    key   = "Environment"
+    value = "prod"
+  }
+
+}
+
 resource "aws_s3_bucket" "cellxgene_data" {
   bucket = "cellxgene-data"
   tags = {
-    cdsi-app = "cellxgene"
+    cdsi-app   = "cellxgene"
     cdsi-owner = "hweej@mskcc.org"
   }
 }
