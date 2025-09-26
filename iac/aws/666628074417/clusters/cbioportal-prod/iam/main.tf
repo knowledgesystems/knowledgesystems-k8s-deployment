c8a91d3b9591091d11cc51058ba8432fb16a9b43
Zain Nasir <zainasir1999@gmail.com>
2025-09-26 14:49:40 -0400

    add s3 mountpoint for cellxgene

---
+++ iac/aws/666628074417/clusters/cbioportal-prod/iam/main.tf
@@ -0,0 +1,92 @@
+locals {
+  account_id                     = data.aws_caller_identity.current.account_id
+  account_region = var.aws_region
+  cluster_name = var.cluster_name
+  permissions_boundary_policy    = "AutomationOrUserServiceRolePermissions"
+  cellxgene_s3_mountpoint_bucket = "cellxgene-data-msk"
+}
+
+data "aws_caller_identity" "current" {}
+
+# S3 mountpoint policy
+resource "aws_iam_policy" "userServicePolicyCellxgeneS3Mountpoint" {
+  name        = "userServicePolicyCellxgeneS3Mountpoint"
+  path        = "/"
+  description = "Policy to allow S3 Mountpoint CSI cluster add-on to access S3 buckets"
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "MountpointFullBucketAccess",
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:ListBucket"
+          ],
+          "Resource" : [
+            "arn:aws:s3:::${local.cellxgene_s3_mountpoint_bucket}"
+          ]
+        },
+        {
+          "Sid" : "MountpointFullObjectAccess",
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:GetObject",
+            "s3:PutObject",
+            "s3:AbortMultipartUpload",
+            "s3:DeleteObject"
+          ],
+          "Resource" : [
+            "arn:aws:s3:::${local.cellxgene_s3_mountpoint_bucket}/*"
+          ]
+        }
+      ]
+    }
+  )
+
+  tags = {
+    cdsi-owner = "hweej@mskcc.org"
+    cdsi-app   = "cellxgene"
+    cdsi-team  = "data-visualization"
+  }
+}
+
+# S3 mountpoint role
+resource "aws_iam_role" "userServiceRoleCellxgeneS3Mountpoint" {
+  name = "userServiceRoleCellxgeneS3Mountpoint"
+
+  assume_role_policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = "sts:AssumeRoleWithWebIdentity",
+          Principal = {
+            Federated = "arn:aws:iam::${local.account_id}:oidc-provider/${var.cluster_oidc_provider_arn}"
+          },
+          Condition = {
+            StringEquals = {
+              "${var.cluster_oidc_provider_arn}:aud" = "sts.amazonaws.com"
+            }
+          }
+        }
+      ]
+    }
+  )
+
+  permissions_boundary = "arn:aws:iam::${local.account_id}:policy/${local.permissions_boundary_policy}"
+
+  tags = {
+    cdsi-owner = "hweej@mskcc.org"
+    cdsi-app   = "cellxgene"
+    cdsi-team  = "data-visualization"
+  }
+}
+
+# S3 mountpoint policy attachment
+resource "aws_iam_role_policy_attachment" "userServicePolicyAttachmentCellxgeneS3Mountpoint" {
+  policy_arn = aws_iam_policy.userServicePolicyCellxgeneS3Mountpoint.arn
+  role       = aws_iam_role.userServiceRoleCellxgeneS3Mountpoint.name
+}
