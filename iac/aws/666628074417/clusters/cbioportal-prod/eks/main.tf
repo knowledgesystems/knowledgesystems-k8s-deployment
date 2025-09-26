c06ad1a0b2ce5b8e63c7138ece38d3e0c42441eb
Zain Nasir <zainasir1999@gmail.com>
2025-09-26 14:28:54 -0400

    init cellxgene

--- iac/aws/666628074417/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/666628074417/clusters/cbioportal-prod/eks/main.tf
@@ -96,6 +96,31 @@ locals {
         (var.LABEL_KEY) = "airflow"
       }
     }
+    cellxgene = {
+      instance_types = ["m7g.xlarge"]
+      ami_type       = "BOTTLEROCKET_ARM_64"
+      desired_size   = 2
+      min_size       = 2
+      max_size       = 2
+      block_device_mappings = {
+        root_vol = var.ROOT_VOL_CONFIG
+      }
+      taints = {
+        dedicated = {
+          key    = var.TAINT_KEY
+          value  = "cellxgene"
+          effect = var.TAINT_EFFECT
+        }
+      }
+      labels = {
+        (var.LABEL_KEY) = "cellxgene"
+      }
+      tags = {
+        cdsi-app   = "cellxgene"
+        cdsi-team  = "data-visualization"
+        cdsi-owner = "hweej@mskcc.org"
+      }
+    }
   }
 }
 
