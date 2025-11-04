9e5e43ffbf9f4c458fed761dda9a6a5d6e4ab93e
Zain Nasir <zainasir1999@gmail.com>
2025-11-04 10:52:27 -0500

    add oncotree to argocd

--- iac/aws/666628074417/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/666628074417/clusters/cbioportal-prod/eks/main.tf
@@ -174,6 +174,23 @@ locals {
         (var.LABEL_KEY) = "prometheus"
       }
     }
+    oncotree = {
+      instance_types = ["t4g.medium"]
+      ami_type       = "BOTTLEROCKET_ARM_64"
+      desired_size   = 1
+      max_size       = 1
+      min_size       = 1
+      taints = {
+        dedicated = {
+          key    = var.TAINT_KEY
+          value  = "oncotree"
+          effect = var.TAINT_EFFECT
+        }
+      }
+      labels = {
+        (var.LABEL_KEY) = "oncotree"
+      }
+    }
   }
 }
 
