1b54aff11d6af36a7c6396d8bce93fc2409c5c11
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2025-12-10 16:12:39 -0500

    Update oncokb-cron nodegroup to t4g.small

--- iac/aws/203403084713/clusters/cbioportal-prod/eks/fargate.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/eks/fargate.tf
@@ -7,10 +7,10 @@ resource "aws_eks_fargate_profile" "cbioportal-dev" {
   cluster_name           = local.cluster_name
   fargate_profile_name   = "cbioportal-dev"
   pod_execution_role_arn = module.iam.fargate_pod_execution_role_arn
-  subnet_ids = var.SUBNET_IDS
+  subnet_ids             = var.SUBNET_IDS
 
   selector {
     namespace = var.FARGATE_SELECTOR_NAMESPACE
-    labels = var.FARGATE_SELECTOR_LABELS
+    labels    = var.FARGATE_SELECTOR_LABELS
   }
 }
