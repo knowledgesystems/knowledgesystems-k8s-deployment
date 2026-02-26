4d51850b991ebcbb6dfc8761a3c06d1a8702567a
Xiang Li <xiangli5699@gmail.com>
2026-02-26 14:09:18 -0500

    Add gn mongodb grch38 clone (#391)

--- iac/aws/203403084713/clusters/cbioportal-prod/ec2/main.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/ec2/main.tf
@@ -16,3 +16,22 @@ resource "aws_ebs_volume" "gn-mongo-v1dot0-mongodb" {
     prevent_destroy = true
   }
 }
+
+resource "aws_ebs_volume" "gn-mongo-v0dot32-grch38-ensembl95-mongodb" {
+  availability_zone = var.GENOMENEXUS_EBS_VOLUME_AZ
+  snapshot_id       = "snap-09872413811317dc9"
+  size              = 500
+  type              = "gp3"
+
+  tags = {
+    Name          = "gn-mongo-v0dot32-grch38-ensembl95-mongodb"
+    cdsi-app      = "genome-nexus"
+    cdsi-team     = "data-engineering"
+    cdsi-owner    = "lix2@mskcc.org"
+    resource-name = "gn-mongo-v0dot32-grch38-ensembl95-mongodb"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
