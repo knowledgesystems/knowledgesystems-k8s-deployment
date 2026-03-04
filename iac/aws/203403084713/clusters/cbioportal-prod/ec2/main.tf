14ec668dc8028ac421634128083300c4f02fd391
Xiang Li <xiangli5699@gmail.com>
2026-03-04 12:35:48 -0500

    Add tf for gnap genie and vep111 GN (#393)

--- iac/aws/203403084713/clusters/cbioportal-prod/ec2/main.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/ec2/main.tf
@@ -64,3 +64,72 @@ resource "aws_ebs_volume" "gn-mongo-v1dot2-grch37-ensembl111-mongodb" {
 output "gn-mongo-v1dot2-grch37-ensembl111_volume_id" {
   value = aws_ebs_volume.gn-mongo-v1dot2-grch37-ensembl111-mongodb.id
 }
+
+resource "aws_ebs_volume" "gn-mongo-vep111-mongodb" {
+  availability_zone = var.GENOMENEXUS_EBS_VOLUME_AZ
+  size              = 150
+  type              = "gp3"
+  snapshot_id       = "snap-007ecb59d110c39ec"
+
+  tags = {
+    Name          = "gn-mongo-vep111-mongodb"
+    cdsi-app      = "genome-nexus"
+    cdsi-team     = "data-engineering"
+    cdsi-owner    = "lix2@mskcc.org"
+    resource-name = "gn-mongo-vep111-mongodb"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "gn_mongo_vep111_volume_id" {
+  value = aws_ebs_volume.gn-mongo-vep111-mongodb.id
+}
+
+resource "aws_ebs_volume" "gn-mongo-gnap-mongodb" {
+  availability_zone = var.GENOMENEXUS_EBS_VOLUME_AZ
+  size              = 100
+  type              = "gp3"
+  snapshot_id       = "snap-080b041320a36270d"
+
+  tags = {
+    Name          = "gn-mongo-gnap-mongodb"
+    cdsi-app      = "genome-nexus"
+    cdsi-team     = "data-engineering"
+    cdsi-owner    = "lix2@mskcc.org"
+    resource-name = "gn-mongo-gnap-mongodb"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "gn_mongo_gnap_volume_id" {
+  value = aws_ebs_volume.gn-mongo-gnap-mongodb.id
+}
+
+resource "aws_ebs_volume" "gn-mongo-genie-mongodb" {
+  availability_zone = var.GENOMENEXUS_EBS_VOLUME_AZ
+  size              = 100
+  type              = "gp3"
+  snapshot_id       = "snap-0617b81fca0195df7"
+
+  tags = {
+    Name          = "gn-mongo-genie-mongodb"
+    cdsi-app      = "genome-nexus"
+    cdsi-team     = "data-engineering"
+    cdsi-owner    = "lix2@mskcc.org"
+    resource-name = "gn-mongo-genie-mongodb"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "gn_mongo_genie_volume_id" {
+  value = aws_ebs_volume.gn-mongo-genie-mongodb.id
+}
