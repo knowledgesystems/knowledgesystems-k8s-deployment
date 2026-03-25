be5265ff9577f5164db417e9e22c350fed6511db
Zain Nasir <zainasir1999@gmail.com>
2026-03-25 17:34:23 -0400

    finish v1.33 upgrade

--- iac/aws/203403084713/clusters/cbioportal-prod/eks/variables.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/eks/variables.tf
@@ -36,7 +36,7 @@ variable "CLUSTER_VER" {
 variable "NODEGROUP_VER" {
   description = "Kubernetes version of the nodegroups"
   type        = string
-  default     = "1.32"
+  default     = "1.33"
 }
 
 variable "CLUSTER_ENV" {
