e09340acbdc7733db60e7729ff3e6010a82ef773
Zain Nasir <zainasir1999@gmail.com>
2026-03-26 11:49:32 -0400

    downscale cbioagent-db

--- iac/aws/666628074417/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/666628074417/clusters/cbioportal-prod/eks/main.tf
@@ -140,7 +140,7 @@ locals {
       }
     }
     cbioagent-db = {
-      instance_types = ["r7i.large"]
+      instance_types = ["t3.small"]
       ami_type       = "BOTTLEROCKET_x86_64"
       desired_size   = 1
       max_size       = 1
