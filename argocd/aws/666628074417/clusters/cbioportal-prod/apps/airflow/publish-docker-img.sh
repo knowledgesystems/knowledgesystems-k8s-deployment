cf66344256a78ba07060bde35e71cd1786276aa3
James Ko <jamesqko@gmail.com>
2026-04-03 14:20:38 -0400

    update script for publishing docker img

--- argocd/aws/666628074417/clusters/cbioportal-prod/apps/airflow/publish-docker-img.sh
+++ argocd/aws/666628074417/clusters/cbioportal-prod/apps/airflow/publish-docker-img.sh
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 
-IMAGE_NAME=jamesko0/cmo-airflow:2.9.2
+VERSION=$1
+
+IMAGE_NAME=cbioportal/airflow-server:$VERSION
 TARGET_PLATFORM=linux/amd64
-DOCKER_HUB_IMG=jamesko0/cmo-airflow:2.9.2
 
 docker buildx build --platform=$TARGET_PLATFORM -t "$IMAGE_NAME" .
 docker login
-docker push $DOCKER_HUB_IMG
+docker push $IMAGE_NAME
