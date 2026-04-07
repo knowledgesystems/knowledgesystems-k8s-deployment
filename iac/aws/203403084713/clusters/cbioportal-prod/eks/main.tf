b2c6203bb62d9a2f7fdec904ced31a074e616af9
Zain Nasir <zainasir1999@gmail.com>
2026-04-07 15:06:18 -0400

    use ec2 with more ips for cbio-api

--- iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
@@ -485,7 +485,7 @@ locals {
       }
     }
     cbio-api = {
-      instance_types = ["r7g.medium"]
+      instance_types = ["t4g.large"]
       ami_type       = "BOTTLEROCKET_ARM_64"
       desired_size   = 2
       max_size       = 2
