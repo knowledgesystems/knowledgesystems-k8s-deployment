09892e2c0fbda1acaae5410ab7ddcc7ccb72c796
Zain Nasir <zainasir1999@gmail.com>
2026-04-15 15:26:18 -0400

    add datahub-databricks connection roles/policies to iam

--- iac/aws/203403084713/shared/iam/main.tf
+++ iac/aws/203403084713/shared/iam/main.tf
@@ -56,3 +56,137 @@ resource "aws_iam_role_policy" "github-lfs-s3-access" {
     ]
   })
 }
+
+# --- Databricks Pipelines Role & Policies ---
+
+resource "aws_iam_role" "userServiceRole-pipelines-databricks" {
+  name        = "userServiceRole-pipelines-databricks"
+  description = "Role for Databricks Unity Catalog to access the cbioportal datahub bucket"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = [
+            "arn:aws:iam::${local.account_id}:role/userServiceRole-pipelines-databricks",
+            var.DATABRICKS_UNITY_CATALOG_ROLE_ARN
+          ]
+        }
+        Action = "sts:AssumeRole"
+        Condition = {
+          StringEquals = {
+            "sts:ExternalId" = var.DATABRICKS_EXTERNAL_ID
+          }
+        }
+      }
+    ]
+  })
+
+  permissions_boundary = "arn:aws:iam::${local.account_id}:policy/${local.permissions_boundary_policy}"
+}
+
+resource "aws_iam_policy" "userServicePolicy-pipelineBucketAccess" {
+  name        = "userServicePolicy-pipelineBucketAccess"
+  description = "S3 read/write/delete access to the datahub bucket for Databricks pipelines"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+        ]
+        Resource = [
+          data.aws_s3_bucket.github-lfs.arn,
+          "${data.aws_s3_bucket.github-lfs.arn}/*"
+        ]
+      },
+      {
+        Effect   = "Allow"
+        Action   = "sts:AssumeRole"
+        Resource = "arn:aws:iam::${local.account_id}:role/userServiceRole-pipelines-databricks"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "userServicePolicy-pipelinesBucketNotifications" {
+  name        = "userServicePolicy-pipelinesBucketNotifications"
+  description = "S3 event notifications via SNS/SQS for the datahub bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ManagedFileEventsSetupStatement"
+        Effect = "Allow"
+        Action = [
+          "s3:GetBucketNotification",
+          "s3:PutBucketNotification",
+          "sns:ListSubscriptionsByTopic",
+          "sns:GetTopicAttributes",
+          "sns:SetTopicAttributes",
+          "sns:CreateTopic",
+          "sns:TagResource",
+          "sns:Publish",
+          "sns:Subscribe",
+          "sqs:CreateQueue",
+          "sqs:DeleteMessage",
+          "sqs:ReceiveMessage",
+          "sqs:SendMessage",
+          "sqs:GetQueueUrl",
+          "sqs:GetQueueAttributes",
+          "sqs:SetQueueAttributes",
+          "sqs:TagQueue",
+          "sqs:PurgeQueue",
+          "sqs:ChangeMessageVisibility"
+        ]
+        Resource = [
+          data.aws_s3_bucket.github-lfs.arn,
+          "arn:aws:sqs:*:*:*",
+          "arn:aws:sns:*:*:*"
+        ]
+      },
+      {
+        Sid    = "ManagedFileEventsListStatement"
+        Effect = "Allow"
+        Action = [
+          "sqs:ListQueues",
+          "sqs:ListQueueTags",
+          "sns:ListTopics"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ManagedFileEventsTeardownStatement"
+        Effect = "Allow"
+        Action = [
+          "sns:Unsubscribe",
+          "sns:DeleteTopic",
+          "sqs:DeleteQueue"
+        ]
+        Resource = [
+          "arn:aws:sqs:*:*:*",
+          "arn:aws:sns:*:*:*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "pipelines-databricks-bucket-access" {
+  role       = aws_iam_role.userServiceRole-pipelines-databricks.name
+  policy_arn = aws_iam_policy.userServicePolicy-pipelineBucketAccess.arn
+}
+
+resource "aws_iam_role_policy_attachment" "pipelines-databricks-bucket-notifications" {
+  role       = aws_iam_role.userServiceRole-pipelines-databricks.name
+  policy_arn = aws_iam_policy.userServicePolicy-pipelinesBucketNotifications.arn
+}
