137089d74b93e0f33a25990aae474b1b5cca4780
Zain Nasir <zainasir1999@gmail.com>
2026-04-28 12:47:02 -0400

    remove datadog nodegroup

--- iac/aws/666628074417/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/666628074417/clusters/cbioportal-prod/eks/main.tf
@@ -25,23 +25,6 @@ locals {
         (var.LABEL_KEY) = "ingress"
       }
     }
-    datadog = {
-      instance_types = ["t3.small"]
-      ami_type       = "BOTTLEROCKET_x86_64"
-      desired_size   = 1
-      min_size       = 1
-      max_size       = 1
-      taints = {
-        dedicated = {
-          key    = var.TAINT_KEY
-          value  = "datadog"
-          effect = var.TAINT_EFFECT
-        }
-      }
-      labels = {
-        (var.LABEL_KEY) = "datadog"
-      }
-    }
     redis = {
       instance_types = ["t3.medium"]
       ami_type       = "BOTTLEROCKET_x86_64"
