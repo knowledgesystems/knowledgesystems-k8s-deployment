b24905b8d4a0671c912f2d21fcc8bd2c24545977
Zain Nasir <zainasir1999@gmail.com>
2026-05-15 15:23:33 -0400

    reduce genome nexus node count to 2

--- iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/203403084713/clusters/cbioportal-prod/eks/main.tf
@@ -243,9 +243,9 @@ locals {
     genome-nexus = {
       instance_types = ["r7i.xlarge"]
       ami_type       = "BOTTLEROCKET_x86_64"
-      desired_size   = 3
+      desired_size   = 2
       min_size       = 2
-      max_size       = 3
+      max_size       = 2
       block_device_mappings = {
         root_vol = var.ROOT_VOL_CONFIG
         data_vol = var.DATA_VOL_CONFIG
