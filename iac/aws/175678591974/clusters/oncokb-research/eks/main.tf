792ea844384a15b013715984fbfb815f429ea801
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2026-06-10 11:46:37 -0400

    Upgrade test nodegroup for oncokb

--- iac/aws/175678591974/clusters/oncokb-research/eks/main.tf
+++ iac/aws/175678591974/clusters/oncokb-research/eks/main.tf
@@ -183,7 +183,7 @@ locals {
       }
     }
     test = {
-      instance_types = ["t4g.large"]
+      instance_types = ["t4g.2xlarge"]
       ami_type       = "BOTTLEROCKET_ARM_64"
       desired_size   = 1
       max_size       = 1
