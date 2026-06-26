51f7ce7028e40c0c52221f6649ae8bc1bc72eab2
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2026-06-26 16:12:15 -0400

    Update desired and max size for airflow instance for hackathon testing

--- iac/aws/666628074417/clusters/cbioportal-prod/eks/main.tf
+++ iac/aws/666628074417/clusters/cbioportal-prod/eks/main.tf
@@ -62,9 +62,9 @@ locals {
     airflow = {
       instance_types = ["m5.large"]
       ami_type       = "BOTTLEROCKET_x86_64"
-      desired_size   = 1
+      desired_size   = 3
       min_size       = 1
-      max_size       = 1
+      max_size       = 3
       subnet_ids     = ["subnet-0917a96517f3dad1d"]
       block_device_mappings = {
         root_vol = var.ROOT_VOL_CONFIG
